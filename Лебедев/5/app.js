c5f20248c8f9395114a33fc5d33f046f1f85700c
Lebedev Alex <winden999@gmail.com>
2018-03-19 03:28:48 +0300

    lab5

--- Лебедев/5/app.js
+++ Лебедев/5/app.js
@@ -8,6 +8,8 @@ const nodemailer = require("nodemailer");
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.raw({ type: 'multipart/form-data' }))
 
+let lastFile = '';
+
 app.get('/', (req, res) => {
     var html = fs.readFileSync('./index.html');
     res.end(html);
@@ -36,9 +38,66 @@ app.get('/domain', (req, res) => {
     });
 });
 
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.end("done");
+app.post('/file', (req, res) => {
+    let arrName = req.query.fname.split('\\');
+    let fname = arrName[arrName.length - 1]; 
+    let filePath = __dirname + '/' + fname;
+    let sendTo = req.query.to;
+    let text = req.query.text;
+
+    fs.appendFile(filePath, req.body, function() {
+        sendEmail(fname, sendTo, text);
+        res.end("done");
+    });
 });
+
+function sendEmail(file_name, sendTo, text) {
+    let transporter = nodemailer.createTransport(
+        {
+            host: 'smtp.yandex.ru',
+            port: 465,
+            secure: true,
+            auth: {
+                user: '',
+                pass: ''
+            }
+        },
+        {
+            // default message fields
+            // sender info
+            from: ''
+        }
+    );
+
+    let message = {
+        // Comma separated list of recipients
+        to: sendTo,
+    
+        // Subject of the message
+        subject: 'Test mail',
+    
+        // plaintext body
+        text: text,
+        // An array of attachments
+        attachments: [
+            // Binary Buffer attachment
+            {
+                filename: file_name,
+                path: __dirname + "/" + file_name,
+            }
+        ]
+    };
+    
+    transporter.sendMail(message, (error, info) => {
+        if (error) {
+            console.log('Error occurred');
+            console.log(error.message);
+            return process.exit(1);
+        }
+    
+        console.log(info.messageId);
+        console.log(info.message.toString());
+    });
+}
 
 app.listen(8000);
